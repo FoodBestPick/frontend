d522431deb0f09a0bdad69156474c28ee50c9f7f
NamDoHyeon2 <skaehgus119@gmail.com>
2025-11-24 21:16:22 +0900

    feat : 구글 소셜로그인 기능 수정 및 네이버 소셜로그인 기능 구현

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,11 @@
 declare module '@env' {
   export const KAKAO_JAVASCRIPT_KEY: string;
   export const KAKAO_REST_API_KEY: string;
+  export const GOOGLE_CLIENT_ID: string;
   export const GOOGLE_WEB_CLIENT_ID: string;
+
+  export const NAVER_CLIENT_ID: string;
+  export const NAVER_CLIENT_SECRET: string;
+  export const NAVER_APP_NAME: string;
+  export const LOCAL_HOST: string;
 }
